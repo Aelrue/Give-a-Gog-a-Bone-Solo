90440ffe025eb44dd23dcc74442ae08256dcded0
Aelrue <101987454+Aelrue@users.noreply.github.com>
2023-02-28 14:30:39 -0600

    added basics to AddToy, imported it in App, and created a button in UserPage that brings user to Add Toy page on click

---
+++ src/components/AddToy/AddToy.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function AddToy() {
+  return (
+    <div className="container">
+      <p>Add Toy Page</p>
+    </div>
+  );
+}
+
+export default AddToy;
--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,15 +1,26 @@
-import React from 'react';
-import LogOutButton from '../LogOutButton/LogOutButton';
-import {useSelector} from 'react-redux';
+import React from "react";
+import LogOutButton from "../LogOutButton/LogOutButton";
+import AddToy from "../AddToy/AddToy";
+import { useHistory } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
+  const history = useHistory();
   return (
     <div className="container">
       <h2>Welcome, {user.username}!</h2>
       <p>Your ID is: {user.id}</p>
-      <LogOutButton className="btn" />
+      <button
+        type="button"
+        className="btn btn_asLink"
+        onClick={() => {
+          history.push("/addtoy");
+        }}
+      >
+        Add Toy
+      </button>
     </div>
   );
 }
