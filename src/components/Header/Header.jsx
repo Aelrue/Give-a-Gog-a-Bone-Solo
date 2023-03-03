0f3d2713fe0f5696449554ff9cda734d1622e1bd
Aelrue <101987454+Aelrue@users.noreply.github.com>
2023-03-03 14:32:13 -0600

    added images to public images folder, created Header function and css, image now appearing as header on all pages

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <header>
+      <div id="header_image">
+        <img src="images/tennis1.jpg"></img>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
