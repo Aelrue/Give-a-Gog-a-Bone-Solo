5054f2234dac20eb8da211b4d60ff8fa35ac846a
Aelrue <101987454+Aelrue@users.noreply.github.com>
2023-03-11 14:55:45 -0600

    commented out all Header code to start fresh with it. Inserted old nav code into .nav to shift elements back into original position

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,14 @@
-import React from "react";
-import "./Header.css";
+// import React from "react";
+// import "./Header.css";
 
-function Header() {
-  return (
-    <header>
-      <div id="header">
-        <img src="images/tennis1.jpg"></img>
-      </div>
-    </header>
-  );
-}
+// function Header() {
+//   return (
+//     <header>
+//       <div id="header">
+//         <img src="images/tennis1.jpg"></img>
+//       </div>
+//     </header>
+//   );
+// }
 
-export default Header;
+// export default Header;
